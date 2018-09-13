21562e54ba05e65560cc5943456b9766f93a1715
Nyagirl <36665395+Nyagirl@users.noreply.github.com>
2018-09-13 15:38:26 +0200

    Add image method

--- Monopoly/Models/Cases/TaxCase.cs
+++ Monopoly/Models/Cases/TaxCase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace Monopoly.Models.Cases
 {
     class TaxCase : AbstractCase
     {
+        private Image cacheTaxImage = null;
+
         public enum Tax { IncomeTax = 100, LuxuryTax = 1000 }
 
         public Tax Type { get; private set; }
@@ -21,5 +24,36 @@ namespace Monopoly.Models.Cases
         }
 
         public override void Land(Game game) { }
+
+        public override Image GetBoardCaseImage()
+        {
+            if (cacheTaxImage != null)
+                return cacheTaxImage;
+
+            Image img = base.GetBoardCaseImage();
+
+            using (Graphics g = Graphics.FromImage(img))
+            {
+                RectangleF rectangle = g.VisibleClipBounds;
+
+                string name = "Taxe";
+                int spaceIndex = name.LastIndexOf(' ');
+                if (spaceIndex != -1)
+                {
+                    char[] n = name.ToCharArray();
+                    n[spaceIndex] = '\n';
+                    name = new string(n);
+                }
+
+                g.DrawString(name, new Font("Arial", 12), Brushes.White, new PointF(rectangle.X + 5, 15));
+
+                g.DrawImage(Properties.Resources.Flouzz, new RectangleF(rectangle.X + 60, 63, 12, 12));
+                g.DrawString("PRIX :    " + Amount.ToString(), new Font("Arial", 12), Brushes.White, new PointF(rectangle.X + 5, 60));
+                // ATTENTION : image à changer 
+                g.DrawImage(Properties.Resources.Gare, new RectangleF(rectangle.X + 10, 73, rectangle.Width - 20, rectangle.Width - 20));
+            }
+
+            return img;
+        }
     }
 }
--- Monopoly/Models/Cases/UtilityProperty.cs
+++ Monopoly/Models/Cases/UtilityProperty.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,41 @@ namespace Monopoly.Models.Cases
     {
         const int UTILITY_PRICE = 500;
 
+        private Image cacheUtilityImage = null;
+
         public UtilityProperty(string name) : base(name, UTILITY_PRICE)
         {
 		}
+
+        public override Image GetBoardCaseImage()
+        {
+            if (cacheUtilityImage != null)
+                return cacheUtilityImage;
+
+            Image img = base.GetBoardCaseImage();
+
+            using (Graphics g = Graphics.FromImage(img))
+            {
+                RectangleF rectangle = g.VisibleClipBounds;
+
+                string name = Name;
+                int spaceIndex = name.LastIndexOf(' ');
+                if (spaceIndex != -1)
+                {
+                    char[] n = name.ToCharArray();
+                    n[spaceIndex] = '\n';
+                    name = new string(n);
+                }
+
+                g.DrawString(name, new Font("Arial", 12), Brushes.White, new PointF(rectangle.X + 5, 15));
+
+                g.DrawImage(Properties.Resources.Flouzz, new RectangleF(rectangle.X + 60, 63, 12, 12));
+                g.DrawString("PRIX :    " + UTILITY_PRICE.ToString(), new Font("Arial", 12), Brushes.White, new PointF(rectangle.X + 5, 60));
+                // ATTENTION : image à changer 
+                g.DrawImage(Properties.Resources.Gare, new RectangleF(rectangle.X + 10, 73, rectangle.Width - 20, rectangle.Width - 20));
+            }
+
+            return img;
+        }
     }
 }
