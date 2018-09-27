3c596de511f6a1e3eff56559fbfc54294f645f29
Nicolas Ettlin <webmaster@nicolapps.ch>
2018-09-27 14:06:07 +0200

    Avoid a game crash on a full board turn

--- Monopoly/Views/Pawn.cs
+++ Monopoly/Views/Pawn.cs
@@ -42,8 +42,8 @@ namespace Monopoly.Views
 
         private void Player_CurrentCaseChanged(Player player, int oldCaseIndex, int newCaseIndex)
         {
-            Position.TransitionDuration = (newCaseIndex - oldCaseIndex) * 0.5;
             Position.Set(GameView.GetCaseOnPlayersPath(newCaseIndex));
+            Position.TransitionDuration = Math.Abs(newCaseIndex - oldCaseIndex) * 0.5;
         }
 
         /// <summary>
